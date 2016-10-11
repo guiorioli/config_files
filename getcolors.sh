51bf99dbbdf74c78a8925b4f7baf3551472ad414
Guilherme Orioli <guilherme.orioli@jetsoft.com.br>
2016-10-11 11:24:10 -0300

    Updated 'usage'

--- getcolors.sh
+++ getcolors.sh
@@ -3,4 +3,7 @@ if [ ! -z $1 ]; then
   cp $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.ui.prefs .
   cp $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.editors.prefs .
   cp $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.wst.jsdt.ui.prefs .
+else
+  echo "Usage:"
+  echo "./install.sh {workspace_directory}"
 fi
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [ ! -z $1 ]; then
-    cp org.eclipse.jdt.ui.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
-    cp org.eclipse.ui.editors.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
-    cp org.eclipse.wst.jsdt.ui.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
+  cp org.eclipse.jdt.ui.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
+  cp org.eclipse.ui.editors.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
+  cp org.eclipse.wst.jsdt.ui.prefs $1/.metadata/.plugins/org.eclipse.core.runtime/.settings/
+else
+  echo "Usage:"
+  echo "./install.sh {workspace_directory}"
 fi
